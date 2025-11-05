06861927c2e3b72206434575e857e8584b67878a
Steve Schofield <steve.schofield@gmail.com>
2025-11-04 21:25:28 -0500

    Change pyenv initialization from bash to zsh

--- resources/pyenv.sh
+++ resources/pyenv.sh
@@ -8,7 +8,7 @@
 curl https://pyenv.run | bash
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.profile
 echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.profile
-echo 'eval "$(pyenv init - bash)"' >> ~/.profile
+echo 'eval "$(pyenv init - zsh)"' >> ~/.profile
 echo 'eval "$(pyenv virtualenv-init -)"' >> ~/.profile
 source ~/.profile
 pyenv install 3.13.0
